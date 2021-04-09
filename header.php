eda899c3236e1a5c852206c7d7d54a0360d01874
ca2roaldsoy <roaldsoyen@gmail.com>
2021-04-09 13:36:17 +0200

    changed classname on nav menus

--- header.php
+++ header.php
@@ -8,12 +8,12 @@
             <h1 class="title"> <a href="<?php echo site_url("/"); ?>">Gamer Revolution</a></h1>
             <nav role="navigation" class="nav">
                 <ul class="nav__items">
-                    <li class="nav__items--link"><a href="<?php echo site_url("/playstation-5"); ?>">PS5</a></li>
-                    <li class="nav__items--link"><a href="<?php echo site_url("/playstation-4"); ?>">PS4</a></li>
-                    <li class="nav__items--link"><a href="<?php echo site_url("/xbox-x-s"); ?>">Xbox Series X/S</a></li>
-                    <li class="nav__items--link"><a href="<?php echo site_url("/xbox-one"); ?>">Xbox One</a></li>
-                    <li class="nav__items--link"><a href="<?php echo site_url("/nintendo-switch"); ?>">Nintendo Switch</a></li>
-                    <li class="nav__items--link"><a href="<?php echo site_url("/pc"); ?>">PC</a></li>
+                    <li <?php if(is_page('playstation-5')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/playstation-5"); ?>">PS5</a></li>
+                    <li <?php if(is_page('playstation-4')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/playstation-4"); ?>">PS4</a></li>
+                    <li <?php if(is_page('xbox-series-xs')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/xbox-series-xs"); ?>">Xbox Series X/S</a></li>
+                    <li <?php if(is_page('xbox-one')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/xbox-one"); ?>">Xbox One</a></li>
+                    <li <?php if(is_page('nintendo-switch')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/nintendo-switch"); ?>">Nintendo Switch</a></li>
+                    <li <?php if(is_page('pc')) echo 'class="current_menu_item"'?>><a href="<?php echo site_url("/pc"); ?>">PC</a></li>
                 </ul>
             </nav>
     
