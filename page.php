6c373fa77f38f2677af8fac35dd76b484175e3a4
ca2roaldsoy <roaldsoyen@gmail.com>
2021-04-09 13:37:39 +0200

    changed pageID on switch statement. Fixed misspelling

--- page.php
+++ page.php
@@ -2,17 +2,16 @@
 
 get_header();
 
-
 function gameQuery($platform) {
     $paged = get_query_var('paged') ? get_query_var('paged') : 0;
 
-    $gameQuery = new WP_Query(array(
+    $gamesQuery = new WP_Query(array(
         'posts_per_page' => 16,
         'post_type' => 'game',
         'paged' => $paged,
         'meta_query' => array(
             array(
-                'key' => 'platform',
+                'key' => 'platforms',
                 'compare' => 'LIKE',
                 'value' => $platform,
             )
@@ -20,12 +19,14 @@ function gameQuery($platform) {
     ))
 
     ?>
+    <article role="article" class="container-fluid gamePage">
     <h1 class='platformTitle'><?php the_title(); ?></h1>
-    <article role="article" class="container-fluid">
         <div class="row">
             <?php
-            while ($gameQuery->have_posts()) {
-                $gameQuery->the_post(); ?>
+            while ($gamesQuery->have_posts()) {
+                $gamesQuery->the_post();
+                
+                ?>
                 <section class="card-deck col-lg-3">
                     <a href='<?php the_permalink() ?>' class="card-game-link">
                         <div class="card">
@@ -50,11 +51,10 @@ function gameQuery($platform) {
             <?php } ?>
         </div>
     </article>
-    <hr />
 
     <?php
     // Pagination
-    $total_pages = $gameQuery->max_num_pages;
+    $total_pages = $gamesQuery->max_num_pages;
     if($total_pages > 1) {
         $current_page = max(1, get_query_var('paged'));
 
@@ -79,17 +79,17 @@ $url = 'http://' . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ];
 $pageID = url_to_postid( $url );
 
 switch ($pageID) {
-    case 5: return gameQuery('playstation 5'); 
+    case 37: return gameQuery('playstation 5'); 
     break;
-    case 8: return gameQuery('playstation 4');
+    case 39: return gameQuery('playstation 4');
     break;
-    case 10: return gameQuery('xbox series s');
+    case 41: return gameQuery('xbox series s');
     break;
-    case 12: return gameQuery('xbox one');
+    case 43: return gameQuery('xbox one');
     break;
-    case 14: return gameQuery('nintendo switch');
+    case 45: return gameQuery('nintendo switch');
     break;
-    case 16: return gameQuery('pc');
+    case 47: return gameQuery('pc');
     break;
     default: return null;
 }
