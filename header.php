00c61c11d9601eb218cb781377f0ee005443b5e6
ca2roaldsoy <roaldsoyen@gmail.com>
2021-04-11 13:13:11 +0200

    hamburger menu and code cleanup

--- header.php
+++ header.php
@@ -8,27 +8,25 @@
         <header class="container-fluid">
             <h1 class="title"> <a href="<?php echo site_url("/"); ?>">Gamer Revolution</a></h1>
 
-                <nav role="navigation" class="nav-menu"> 
+            <nav role="navigation" class="nav-menu"> 
                 <div id="menuToggle">
-                
-                <ul class="nav__items" id="menu">
-                    <li <?php if(is_page('playstation-5')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/playstation-5"); ?>">PS5</a></li>
-                    <li <?php if(is_page('playstation-4')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/playstation-4"); ?>">PS4</a></li>
-                    <li <?php if(is_page('xbox-series-xs')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/xbox-series-xs"); ?>">Xbox Series X/S</a></li>
-                    <li <?php if(is_page('xbox-one')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/xbox-one"); ?>">Xbox One</a></li>
-                    <li <?php if(is_page('nintendo-switch')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/nintendo-switch"); ?>">Nintendo Switch</a></li>
-                    <li <?php if(is_page('pc')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/pc"); ?>">PC</a></li>
-                </ul>
-                 
+                    <ul class="nav__items" id="menu">
+                        <li <?php if(is_page('playstation-5')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/playstation-5"); ?>">PS5</a></li>
+                        <li <?php if(is_page('playstation-4')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/playstation-4"); ?>">PS4</a></li>
+                        <li <?php if(is_page('xbox-series-xs')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/xbox-series-xs"); ?>">Xbox Series X/S</a></li>
+                        <li <?php if(is_page('xbox-one')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/xbox-one"); ?>">Xbox One</a></li>
+                        <li <?php if(is_page('nintendo-switch')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/nintendo-switch"); ?>">Nintendo Switch</a></li>
+                        <li <?php if(is_page('pc')) echo 'class="current_menu_item"' ?>><a href="<?php echo site_url("/pc"); ?>">PC</a></li>
+                    </ul>    
                 </div> 
-    
-            <input type="search" name="search" class="searchGame" placeholder="Search for a game..." aria-label="Search through site content">
         
-            <section class="hamburgerIcons">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-            </section>
+                <input type="search" name="search" class="searchGame" placeholder="Search for a game..." aria-label="Search through site content">
+            
+                <section class="hamburgerIcons">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                </section>
             </nav>
         </header>
 
